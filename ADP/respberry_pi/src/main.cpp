35d2d842fc79d619bb2ea1dfa813a04b52a63585
TERRIOR <954179799@qq.com>
2016-08-21 10:55:55 +0800

    提高检测精度，添加泡沫比例计算

--- ADP/respberry_pi/src/main.cpp
+++ ADP/respberry_pi/src/main.cpp
@@ -7,10 +7,11 @@ using namespace std;
 #define MIN_GRAY_VALUE 0
 const int g_cannyLowThreshold =3;
 const int c_rectr = 20;
-int getarea(Mat img);
+int getarea(Mat &img);
 int otsu(cv::Mat&dst);
 int otsu(cv::Mat&dst, cv::Mat&mask);
 bool isclose(vector<Point> contour);
+bool comp(const vector<Point> &a, const  vector<Point> &b);
 int main(int argc, char* argv[])
 {
 	Mat frame, gray, mask ,f1;
@@ -33,7 +34,8 @@ int main(int argc, char* argv[])
 	Mat frame_gray,framecp;
 	Mat cmask;
 	Rect re;
-	int c_size;
+	int c_size_max,c_size_min;
+	Mat lastmask,lastmask2;
 	frame.copyTo(framecp);
 	cvtColor(frame, frame_gray, CV_RGB2GRAY);
 	GaussianBlur(frame_gray, frame_gray, Size(35, 35), 0, 0);//高斯必须为奇数
@@ -51,7 +53,7 @@ int main(int argc, char* argv[])
 	vector<vector<Point> >::iterator itc = contours.begin();
 	while (itc != contours.end())
 	{
-		if (itc->size()<cmin || itc->size()>cmax  )
+		if ((!isclose(*itc)) || itc->size()<cmin || itc->size()>cmax)
 			itc = contours.erase(itc);
 		else
 		{
@@ -62,41 +64,43 @@ int main(int argc, char* argv[])
 	}
 	//cout << "Contours: " << contours.size() << "  " << ends;
 	//cout << endl;
-	/////////////////////////////////////
 	if (contours.size() > 0){
 		itc = contours.begin();
 		int i = 0;
-		int j = 0;
-		unsigned int max = itc->size();
-
 		while (itc != contours.end())
 		{
 			Scalar color(40 * i, 255, 30 * i);
 			drawContours(framecp, contours, i, color, 2);
-			if (itc->size() > max){
-				max = itc->size();
-				j = i;
-			}
 			itc++;
 			i++;
 		}
-		//cout << "max:" << max << "第" << j << endl;
-		c_size = max;
-		ellisperectmax = fitEllipse(Mat(contours[j]));
+		sort(contours.begin(), contours.end(), comp);//对轮廓进行排序
+		c_size_max = contours[0].size();//取出最大轮廓
+		c_size_min = contours[1].size();//取出第二大的轮廓
+		ellisperectmax = fitEllipse(Mat(contours[0]));
+		RotatedRect ellispemin = fitEllipse(Mat(contours[1]));
 		cout << "height:" << ellisperectmax.size.height << "width:" << ellisperectmax.size.width << "area:" << ellisperectmax.size.area() << endl;
-		re = boundingRect(contours[j]);//roi感兴趣区域，其他去除
+		re = boundingRect(contours[0]);//roi感兴趣区域，其他去除
 		ellipse(framecp, ellisperectmax, Scalar(rand() & 255, rand() & 255, rand() & 255), 8);
 		imshow("framecp", framecp);
 		cmask = Mat::zeros(frame.size(), CV_8UC1);
+		lastmask=Mat::zeros(frame.size(), CV_8UC1);
+		lastmask2 = Mat::zeros(frame.size(), CV_8UC1);
 		cmask.setTo(0);
+		lastmask.setTo(255);
+		lastmask2.setTo(255);
 		ellipse(cmask, ellisperectmax, Scalar(255), -1);
+		ellipse(lastmask, RotatedRect(ellispemin.center, Size(ellispemin.size.width - 100, ellispemin.size.height -100), ellispemin.angle), Scalar(0), -1);
+		ellipse(lastmask2, RotatedRect(ellispemin.center, Size(ellispemin.size.width +100, ellispemin.size.height + 100), ellispemin.angle), Scalar(0), -1);
 		cmask = cmask(Rect(re.x + 15, re.y + 15, re.size().width - 30, re.size().height - 30));
+		lastmask = lastmask(Rect(re.x + 15, re.y + 15, re.size().width - 30, re.size().height - 30));
+		lastmask2 = lastmask2(Rect(re.x + 15, re.y + 15, re.size().width - 30, re.size().height - 30));
 		ellisperectmax.center.x = ellisperectmax.center.x - re.x-15;//消除切除后圆心的偏移
 		ellisperectmax.center.y = ellisperectmax.center.y - re.y-15;//同上
 		//Mat cmask2 = Mat::zeros(frame.size(), CV_8UC1);
 		//circle(cmask2, c_point, c_rectr, Scalar(255));
 		//imshow("ellipse", cmask);
-		//imshow("ellipse", cmask2);
+		//imshow("ellipse2", lastmask);
 	}
 	while (1)
 	{
@@ -119,11 +123,11 @@ int main(int argc, char* argv[])
 			Vibe_Bgs.testAndUpdate(gray);
 			mask = Vibe_Bgs.getMask();
 			
-			Mat element = getStructuringElement(MORPH_ELLIPSE, Size(7,7));
+			Mat element = getStructuringElement(MORPH_ELLIPSE, Size(15,15));
 			morphologyEx(mask, mask, MORPH_OPEN, element);
 			
-			int mask_area = 0;
-			mask_area=getarea(mask);//这里的mask需要改成只有注水杆区域（目前还包括粉层的扩散）
+			int mask_area = countNonZero(mask);//这里的mask需要改成只有注水杆区域（目前还包括粉层的扩散）
+			//mask_area=getarea(mask);
 			cout << "area:"<<mask_area<< endl;
 
 			if (mask_area<1000)
@@ -136,34 +140,36 @@ int main(int argc, char* argv[])
 			Mat frame2(Scalar(255));
 			frame2.setTo(255);
 			Mat frame_gs;
-			Mat frame3;
-			frame.copyTo(frame3, mask);
+			//Mat frame3;
+			//frame.copyTo(frame3, mask);
 			//imshow("frame3", frame3);
-	
+			
+			/*vibe运动算法去除注水区域（暂时不用）
 			f1.copyTo(frame2, mask);
 			mask = 255 - mask;//mask取反 此时mask为没运动区域
 			frame.copyTo(frame2, mask);
 			Mat frame2_gray;
 			cvtColor(frame2, frame2_gray, CV_RGB2GRAY);
-			//GaussianBlur(frame2_gray, frame2_gray, Size(3, 3), 0, 0);
-			element = getStructuringElement(MORPH_ELLIPSE, Size(12, 12));
-			//进行形态学操作  
-			//morphologyEx(frame2_gray, frame2_gray, MORPH_OPEN, element);
-			Mat dst1;
-			//mask = cmask&mask;//此时mask为椭圆内没运动区域
-			GaussianBlur(gray, gray, Size(13, 13), 0, 0);
-			morphologyEx(gray, gray, MORPH_CLOSE, element);
-			int a = otsu(gray, cmask);//只对cmask二值化（避免四个角剩余的蓝黑色部分影响）
-			cout << "a:" << a<<endl;
-			//Mat gray_roi;//设置roi
-			//gray_roi = gray(re);//把那个区域拿出来；
-			//imshow("roi", gray_roi);
-			gray = gray > a;
-			Mat c_gray;
-			gray.copyTo(c_gray, cmask);
+			GaussianBlur(frame2_gray, frame2_gray, Size(3, 3), 0, 0);
+			element = getStructuringElement(MORPH_ELLIPSE, Size(20, 20));
+			morphologyEx(frame2_gray, frame2_gray, MORPH_OPEN, element);
+			imshow("f2", frame2_gray);//消除遮挡后mat
+			*/
+		
 			
-			//erode(c_gray, c_gray, element);//腐蚀操作
-			//morphologyEx(c_gray, c_gray, MORPH_CLOSE, element);
+			Mat dst1;
+			GaussianBlur(gray, gray, Size(21, 21), 0, 0);
+			//morphologyEx(gray, gray, MORPH_CLOSE, element);
+			//mask = cmask&mask&lastmask;//此时mask为椭圆内没运动区域
+			//imshow("ellispe", mask);
+			int a = otsu(gray, cmask);//只对cmask二值化（避免四个角剩余的蓝黑色部分影响）
+			frame_gs = gray > a;
+			Mat c_gray;
+			frame_gs.copyTo(c_gray, cmask);
+			c_gray = (c_gray&lastmask)|lastmask2;//lastmask与lastmask2限制了粉圈范围，粉圈的二值化图黑圈大于lastmask，小于lastmask2
+			//dilate (c_gray, c_gray, element);//腐蚀操作
+			element = getStructuringElement(MORPH_ELLIPSE, Size(20, 20));
+			morphologyEx(c_gray, c_gray, MORPH_CLOSE, element);
 			imshow("dst", c_gray);
 			Canny(c_gray, c_gray, g_cannyLowThreshold, g_cannyLowThreshold * 3, 3);
 			findContours(c_gray, contours, hierarchy, CV_RETR_EXTERNAL, CV_CHAIN_APPROX_SIMPLE);
@@ -171,15 +177,10 @@ int main(int argc, char* argv[])
 			for (; itContours != contours.end(); ++itContours) {
 
 				cout << "Size: " << itContours->size() << ends;//每个轮廓包含的点数
-				/*vector<Point>::iterator it1 = itContours->begin();
-				for (; it1 != (*itContours).end(); ++it1) {
-					cout << "x:" << it1->x<<"y:"<<it1->y << endl;
-				}*/
-
 			}
-			cout << "Contours: " << contours.size() << endl;
-			unsigned int cmin = 200;
-			unsigned int cmax =500;
+			cout << "Contours: " << contours.size() << c_size_min << endl;
+			unsigned int cmin = c_size_min-100;//把上次检测的结果作为下次检测的筛选条件
+			unsigned int cmax =c_size_min+50;//
 			vector<vector<Point> >::iterator itc = contours.begin();
 			while (itc != contours.end())
 			{
@@ -217,14 +218,29 @@ int main(int argc, char* argv[])
 					&& (rrect.size.height + rrect.size.width)<(ellisperectmax.size.height + ellisperectmax.size.width)
 					&& rrect.size.area()<ellisperectmax.size.area())
 				{
-					
+					c_size_min = contours[j].size();
 					ellipse(framecp, rrect, Scalar(rand() & 255, rand() & 255, rand() & 255), 8);
+					lastmask.setTo(255);
+					lastmask2.setTo(255);
+					//以这次检测出的圆去限制（补全&去除）下一个圆   范围：-10~30（待调整）
+					ellipse(lastmask2, RotatedRect(rrect.center, Size(rrect.size.width +30, rrect.size.height +30), rrect.angle), Scalar(0), -1);
+					ellipse(lastmask, RotatedRect(rrect.center, Size(rrect.size.width - 10, rrect.size.height - 10), rrect.angle), Scalar(0), -1);
+					Rect re = boundingRect(Mat(contours[j]));//roi感兴趣区域，其他去除
+					//int a = otsu(gray);
+					Mat fc= gray(re);
+					fc = fc > 100;//取出单独处理  fc = fc > a;
+					int contarea=contourArea(contours[j]);
+					int blackarea =fc.rows*fc.cols- countNonZero(fc);
+					double prewhite =  (contarea - blackarea)*100/ contarea;
+					cout << prewhite<<"%"<< "  "<<contarea << "  "<<blackarea<<  endl;
+					imshow("quanli", fc);
+					imshow("framecp", framecp);//绘制椭圆轮廓后的图像
 				}
-				imshow("framecp", framecp);//绘制椭圆轮廓后的图像
+				
 			}
-			imshow("f2", frame2_gray);//消除遮挡后mat
+			
 		}
-		//imshow("input", frame);
+		//imshow("input", frame);//原图
 
 		if (cvWaitKey(10) == 27)
 			break;
@@ -233,6 +249,11 @@ int main(int argc, char* argv[])
 	return 0;
 }
 
+
+bool comp(const vector<Point> &a, const  vector<Point> &b)//soft排序的自定义排序函数
+{
+	return a.size()>b.size();
+}
 bool isclose(vector<Point> contour){//判断轮廓是否连续
 	Point firstp;
 	Point endp;
@@ -249,7 +270,7 @@ bool isclose(vector<Point> contour){//判断轮廓是否连续
 		return true;
 	}
 }
-int getarea(Mat img){
+int getarea(Mat &img){
 	int i = 0,j=0;
 	int height = img.cols;
 	int width = img.rows;
@@ -257,7 +278,7 @@ int getarea(Mat img){
 	for (i = 0; i<width; i++){
 		for (j = 0; j<height; j++){
 			
-			if (img.at<char>(i,j)!=0)
+			if (img.at<char>(i,j)=0)
 			{
 				count++;
 			}
@@ -266,7 +287,7 @@ int getarea(Mat img){
 	return count;
 }
 ////////////////////////////////灰度直方图增强对比度///////////////////////////////////////////////////
-int ImgStrong(Mat img, Mat result)
+int ImgStrong(Mat &img, Mat &result)
 {
 	//***************  
 	//p[]各个灰度级出现的概率  

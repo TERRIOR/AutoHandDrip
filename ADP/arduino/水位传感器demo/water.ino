eef911fa3f9d6ede8db2384dfd87b56b8b8d8d8c
TERRIOR <954179799@qq.com>
2016-08-24 16:43:05 +0800

    新增水位计demo

---
+++ ADP/arduino/水位传感器demo/water.ino
@@ -0,0 +1,26 @@
+/*
+红线接5v,黄线为信号线(自行挑选引脚,此处用pin2),蓝线接地,黑线接地
+信号线为低电平时,说明检测到水
+*/
+bool waterstate=LOW;
+const int waterpin=2;
+const int ledpin=13;
+void setup() {
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+  pinMode(waterpin,INPUT);
+  pinMode(ledpin,OUTPUT);
+  
+}
+
+void loop() {
+  // put your main code here, to run repeatedly:
+  waterstate=digitalRead(waterpin);
+  if(waterstate==LOW){
+    digitalWrite(ledpin,HIGH);
+    Serial.println("reached");
+  }else{
+    Serial.println("unreached");
+    digitalWrite(ledpin,LOW);
+  }
+}

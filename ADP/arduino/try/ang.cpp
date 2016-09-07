1ed5055150f3c862103be998fdb2f82187ef8f67
Just-Boom <569141331@qq.com>
2016-09-07 10:50:53 +0800

    修改弧度变角度

--- ADP/arduino/try/ang.cpp
+++ ADP/arduino/try/ang.cpp
@@ -17,19 +17,20 @@ float distance;
 distance=sqrt((x1-x2)*(x1-x2)+(y1-y2)*(y1-y2));
 return distance;
 }*/
-
+//转换坐标
 void changeangle(posi* posi_1,posi* posi_2){
         (posi_2->x) = (posi_1->x)*cos(leg) - (posi_1->z)*sin(leg);
         (posi_2->z) = (posi_1->x)*sin(leg) +(posi_1->z)*cos(leg);
         (posi_2->y) = (posi_1->y);
 }
-  
+//计算坐标  
 float Angle(float x,float y,float z){
  float c=sqrt((x+effectorside-baseside)*(x+effectorside-baseside)+y*y+(z+effectorside-baseside)*(z+effectorside-baseside));
  float a2=shin*shin-z*z;
  float alphy = acos((-a2+thigh*thigh+c*c)/(2*thigh*c));
  float beta = -atan2(y,x);
- float angle = alphy-beta;
+ float i = alphy-beta;
+ float angle = i/6.28*360;     
  return angle;
 }
 
@@ -41,8 +42,12 @@ void xyztoangle(float x,float y,float z,angle *a){
   p1.z=z;
   changeangle(&p1,&p2);
   changeangle(&p2,&p3);
+  Serial.println(p1.x);
+  Serial.println(p1.y);
+  Serial.println(p1.z);
   a->angle1=Angle(p1.x,p1.y,p1.z);
   a->angle2=Angle(p2.x,p2.y,p2.z);
   a->angle3=Angle(p3.x,p3.y,p3.z);
+  //Serial.println(a->angle1);
 
 }
